48d48c8af416359f772b869aadb4ee58c7277aad
HILMY ABDUL AZIZ <127839447+sincosincos@users.noreply.github.com>
2024-01-05 15:13:57 +0700

    Update app.R

--- app.R
+++ app.R
@@ -2,23 +2,7 @@ library(shiny)
 library(shinythemes)
 
 # Memuat Data
-# Membuat dataset berdasarkan data yang diberikan
-dataset <- data.frame(
-  Month = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
-  x1 = c(150000, 160000, 170000, 180000, 190000, 200000, 210000, 220000, 230000, 240000, 250000, 260000),
-  x2 = c(8000, 9500, 10000, 10500, 11000, 9000, 11500, 12000, 12500, 13000, 14000, 15000),
-  x3 = c(5, 4.5, 4.8, 4.6, 5.1, 4.7, 4.9, 5, 5.2, 5.3, 5.4, 5.5),
-  x4 = c(8.5, 8.2, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 8.7, 8.8, 8.9, 9),
-  x5 = c(20000, 22000, 25000, 23000, 30000, 28000, 27000, 35000, 40000, 45000, 50000, 60000),
-  y = c(120, 150, 160, 165, 180, 170, 190, 210, 230, 250, 300, 350)
-)
-# Mengonversi kolom-kolom ke tipe data numerik
-dataset$x1 <- as.numeric(dataset$x1)
-dataset$x2 <- as.numeric(dataset$x2)
-dataset$x3 <- as.numeric(dataset$x3)
-dataset$x4 <- as.numeric(dataset$x4)
-dataset$x5 <- as.numeric(dataset$x5)
-dataset$y <- as.numeric(dataset$y)
+dataset <- read.csv(".../e-commerce.csv", sep = ";")
 
 # Membuat model regresi linier berganda
 sales_model <- lm(y ~ x1 + x2 + x3 + x4 + x5, data = dataset)
