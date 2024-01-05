73b1ba8897a8eca69a8dd454d1cc51c41565c8bf
HILMY ABDUL AZIZ <127839447+sincosincos@users.noreply.github.com>
2024-01-05 15:12:33 +0700

    Update Shinyapp.R

--- Shinyapp.R
+++ Shinyapp.R
@@ -1,4 +1,4 @@
-
-rsconnect::setAccountInfo(name='hilmy', token='08C3F9ECE38ED276452A77AC0BDD600B', secret='URCz34GcDsu9jYuTfGm8C5hmyKdyv+y7YUXhIeDh')
+install.packages('rsconnect')
+rsconnect::setAccountInfo()
 library(rsconnect)
-rsconnect::deployApp((appDir = "D:/Kuliah/Semester 5/Business Intel/UAS BI/e-commerce"))
+rsconnect::deployApp()
